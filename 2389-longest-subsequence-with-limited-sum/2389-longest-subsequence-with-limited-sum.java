17aaab0a272b6fa9be6845f06b890075490ca102
saurabh1505717 <84388992+saurabh1505717@users.noreply.github.com>
2022-12-25 23:09:34 +0530

    Time: 60 ms (5.04%), Space: 43.1 MB (77.45%) - LeetHub

---
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        int ans[]=new int[queries.length];
+        Arrays.sort(nums);
+        
+        int sum=Arrays.stream(nums).sum();
+        System.out.println(sum);
+        for(int i=0; i<queries.length; i++){
+            int j=nums.length-1;
+            int presentSum=sum;
+            while(j>=0 && presentSum>queries[i]){
+                presentSum=presentSum-nums[j];
+                j--;
+            }
+            System.out.println(j);
+            ans[i]=j+1;;
+        }
+        return ans;
+    }
+}
