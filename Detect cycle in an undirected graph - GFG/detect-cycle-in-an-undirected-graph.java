84acd59d66e73648b15bd9b7ca12ae955f66159c
saurabh1505717 <84388992+saurabh1505717@users.noreply.github.com>
2023-03-26 19:41:56 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -64,19 +64,20 @@ class Solution {
     //     return false;
     // }
     
-    private boolean dfs(int node, int parent, int vis[], ArrayList<ArrayList<Integer>> 
-    adj) {
-        vis[node] = 1; 
-        // go to all adjacent nodes
-        for(int adjacentNode: adj.get(node)) {
-            if(vis[adjacentNode]==0) {
-                if(dfs(adjacentNode, node, vis, adj) == true) 
-                    return true; 
+    private boolean detectCycleDFS(int node, int parent,  int []vis, ArrayList<ArrayList<Integer>>adj){
+        vis[node]=1;
+        for(int adjacentNode:adj.get(node)){
+            if(vis[adjacentNode]==0){
+                if(detectCycleDFS(adjacentNode, node, vis, adj)==true){
+                    return true;
+                }
             }
-            // if adjacent node is visited and is not its own parent node
-            else if(adjacentNode != parent) return true; 
+                else if(adjacentNode!=parent){
+                    return true;
+                }
+
         }
-        return false; 
+        return false;
     }
     
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
@@ -91,12 +92,15 @@ class Solution {
         // }
         // return false;
         
-        int vis[] = new int[V]; 
-       for(int i = 0;i<V;i++) {
-           if(vis[i] == 0) {
-               if(dfs(i, -1, vis, adj) == true) return true; 
-           }
-       }
-       return false; 
+        int[] vis=new int[V];
+        // Arrays.fill(vis, false);
+        for(int i=0; i<V; i++){
+            if(vis[i]==0){
+                if(detectCycleDFS(i, -1, vis, adj)==true){
+                    return true;
+                }   
+            }
+        }
+        return false;
     }
 }
