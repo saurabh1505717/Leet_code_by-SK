9b3592a37895f0f63852c020901fdbc7a7151e9f
saurabh1505717 <84388992+saurabh1505717@users.noreply.github.com>
2023-04-07 18:05:41 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.java
@@ -32,70 +32,33 @@ class DriverClass {
 /*Complete the function below*/
 
 class Solution {
-    // private boolean detectCycleDFS(int node, ArrayList<ArrayList<Integer>>adj, int[]vis, int[]pathVis){
-    //     vis[node]=1;
-    //     pathVis[node]=1;
-    //     for(int it:adj.get(node)){
-    //         if(vis[it]==0){
-    //             if(detectCycleDFS(it, adj, vis, pathVis)==true){
-    //                 return true;
-    //             }
-    //         }
-    //         // if a node has been previously vivited and appeared to get visited again 
-    //         else if(pathVis[it]==1){
-    //             return true;
-    //         }
-    //     }
-    //     pathVis[node]=0;
-    //     return false;
-    // }
+    private boolean dfs(int node, ArrayList<ArrayList<Integer>>adj, int []vis, int[] pathVis){
+        vis[node]=1;
+        pathVis[node]=1;
+        for(int adjNode:adj.get(node)){
+            if(vis[adjNode]==0){
+                if(dfs(adjNode, adj, vis, pathVis)==true){
+                    return true;
+                }
+            }
+            else if(vis[adjNode]==1 && pathVis[adjNode]==1){
+                return true; 
+            }
+        }
+        pathVis[node]=0;
+        return false;
+    }
     
-    // Function to detect cycle in a directed graph.
     public boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
-        // int[]vis=new int[V];
-        // int []pathVis=new int[V];
-        // for(int i=0; i<V; i++){
-        //     if(vis[i]==0){
-        //         if(detectCycleDFS(i, adj, vis, pathVis)==true){
-        //             return true;
-        //         }
-        //     }
-        // }
-        // return false;
-        
-        
-        
-        // Now solving this using KAHN'S Algorithm(BFS)
-        int inDegree[]=new int[V];
+        int vis[]=new int[V];
+        int pathVis[]=new int[V];
         for(int i=0; i<V; i++){
-            for(int nbr: adj.get(i)){
-                inDegree[nbr]++;
-            }
-        }
-        
-        Queue<Integer>q=new LinkedList<>();
-        for(int i=0; i<V; i++){
-            if(inDegree[i]==0){
-                q.add(i);
-            }
-        }
-        
-        int count=0;
-        int j=0;
-        while(!q.isEmpty()){
-            int node=q.peek();
-            q.remove();
-            count++;
-            for(int nbrs:adj.get(node)){
-                inDegree[nbrs]--;
-                if(inDegree[nbrs]==0){
-                    q.add(nbrs);
+            if(vis[i]==0){
+                if(dfs(i, adj, vis, pathVis)==true){
+                    return true;
                 }
             }
         }
-        if(count==V){
-            return false;
-        }
-        return true;
+        return false;
     }
 }
